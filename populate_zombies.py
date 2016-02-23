64975f5f3746addb6897b5e0b6068396651c0759
vilisimo <vilisimo@gmail.com>
2016-02-23 19:44:11 +0000

    Some comments, population script stub, admin interface added.

---
+++ populate_zombies.py
@@ -0,0 +1,15 @@
+# We will most likely need a population script so that we can work with the same data.
+
+# We have to import project's settings. When creating the script, uncomment the following lines first:
+#   import os
+#   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zombies_on_campus.settings')
+
+# These two lines import the Django settings. Uncomment these as well
+# (otherwise will not allow to import our models)
+#   import django
+#   django.setup()
+
+# Finally, uncomment and edit this line
+#   from zombies.models import <model 1> <model 2> ... <model n>
+
+# After this is done, population script can be written.
--- zombies/models.py
+++ zombies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# USEFUL INFO:
+# Whenever changes to the models are made, need to register those changes like so:
+#   python manage.py makemigrations <app_name>
+# Then apply them like so:
+#   python manage.py migrate
+
+# To change password of a user (including admin):
+#   manage.py changepassword <user_name>
+
+# Don't forget to import/register models to admin.py
+# http://www.tangowithdjango.com/book17/chapters/models.html#configuring-the-admin-interface
