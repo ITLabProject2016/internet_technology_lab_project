beb7f44fa01bf17f45006275acc4d702c064590e
shallu gangwani <shallugangwani@gmail.com>
2016-03-17 23:17:37 +0000

    Story 3 added

--- populate_zombies_story3.py
+++ populate_zombies_story3.py
@@ -14,36 +14,34 @@ django.setup()
 # Finally, uncomment and edit this line
 from django.core.files import File
 from zombies.models import Story, StoryPoint
-place
+
 
 def populate():
     #the story name has to be unique
     story = add_story("Zombies in Cave", "There is no light but seems like there is fire coming towards you", "dev")
 
     # # Level 1
-    sp1 = add_sp(story, 1, None,  "You are in Boydd Orr, and you have just explored a cave underneath", "dev", None, 10, "start", None)
+    sp1 = add_sp(story, 1, None,  "You are in Boydd Orr, and you have just explored a cave underneath.", "dev", None, 10, "start", None)
 
     # Level 2
     sp2 = add_sp(story, 2, sp1,  "You are delighted to come as you notice that all your classmates are having a party. ",  "dev", "You dare to go in.",10, "mid", None)
-    sp3 = add_sp(story, 3, sp1,  "O no! you missed the party, all your friends were enjoying in that cave. Awesome!!", "dev", "You are in dilemma, you dont think you should go but curious in case you find some hidden treasure", 10, "end", None)
+    sp3 = add_sp(story, 3, sp1,  "O no! you missed the party, all your friends were enjoying in that cave. Awesome!!", "dev", "You are scared, so turn the other way.", 10, "end", None)
 
     # Level 3
-    sp4 = add_sp(story, 4, sp2,  "O so you see Zombies have decided to come and dance with you ", "dev", "Surprise on the dance floor for you.", 10, "mid", "bad")
+    sp4 = add_sp(story, 4, sp2,  "O so you see Zombies have decided to come and dance with you. ", "dev", "Surprise on the dance floor for you.", 10, "mid", "bad")
     sp5 = add_sp(story, 5, sp2,  "O no! you need to rush to the toilet. You got food poisioning.. Aaaaaaaaaaaaah!", "dev", "Have some free food.", 10, "end", None)
-    sp6 = add_sp(story, 6, sp2,  "Someone proposed to rather have a ball dance", "dev", "Time to socialize", 10, "mid", None)
-
-    sp7 = add_sp(story, 8, sp4,  "OOh! Zombies getting too close and you are feeling uncomfortable", "dev", "You happily accept coz you are brave", 10, "mid", None)
-    sp8 = add_sp(story, 9, sp4,  "The academic zombies get you! Bad Luck!", "dev", "You decide to sneakily run", 5, "mid", "bad")
+    sp6 = add_sp(story, 6, sp2,  "Someone proposed you to have a ball dance with you..", "dev", "Time to socialize", 10, "mid", None)
 
     #Level 4
-    sp10 = add_sp(story, 10, sp6,  "The Zombie got you!!!", "dev", "Run faster!!!", 5, "end", "bad")
-    sp11 = add_sp(story, 11, sp6,  "You are in the escape pod with a preset route to earth! you are saved!", "dev", "Close the Gate!", 10,"end", "bad")
+    sp7 = add_sp(story, 7, sp4,  "Suddenly one Zombie pushes you in the deepest end of the cave.", "dev", "you are brave and wouldn't mind", 10, "mid", None)
+    sp8 = add_sp(story, 8, sp4,  "Aha!! You bump into a lecturer who's really hot!!", "dev", "You decide to sneakily run", 5, "end", "bad")
 
-    sp12 = add_sp(story, 12, sp7,  "You got into the escape pod... But the zombie followed you!", "dev", "Try to get in the escape pod", 5, "end", "bad")
-    sp13 = add_sp(story, 13, sp7,  "You kill the Zombie and you escape the station with the escape pods!", "dev", "Kill it now that you have the chance", 10, "end", "good")
+    #Level 5
+    sp9 = add_sp(story, 9, sp6,  "And here the lights are gone and you make the most of your time.", "dev", "You accept", 5, "end", "bad")
+    sp10 = add_sp(story, 10, sp6,  "O no! Too late in deciding and you missed a golden opportunity as your best friend has now taken the offer.", "dev", "you decline because you are embarrased.", 10,"end", "bad")
 
-    sp14 = add_sp(story, 14, sp8,  "You cant find a cure for the zombies because you are a software engineer remeber???", "dev", "On your way around you find the biology lab", 5, "end", "good")
-    sp15 = add_sp(story, 15, sp8,  "You struggle to build a killing Robot with RoboJango! Your robot saves you!!!", "dev", "On your way around you find the robot lab", 5, "end", "bad")
+    sp11 = add_sp(story, 11, sp7,  "You end up in a hole, and stuck forever.", "dev", "You see an exit and move towards it.", 5, "end", "bad")
+    sp12 = add_sp(story, 12, sp7,  "Congratulations!! you are a millionnaire now as you got a solitaire in the treasure..", "dev", "You see treasure on your left and decide to grab before you go.", 10, "end", "good")
 
 
 # sid - story_id; n - name; desc - description
