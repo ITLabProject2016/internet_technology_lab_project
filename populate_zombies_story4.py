58b8d63dc8318c2bf93a41724038efe8dc7990e5
shallu gangwani <shallugangwani@gmail.com>
2016-03-18 01:40:05 +0000

    Story 4 updated

--- populate_zombies_story4.py
+++ populate_zombies_story4.py
@@ -16,6 +16,7 @@ from django.core.files import File
 from zombies.models import Story, StoryPoint
 
 
+
 def populate():
     #the story name has to be unique
     story = add_story("Love Story in Campus", " Wonderful days were those", "dev")
@@ -25,23 +26,26 @@ def populate():
 
     # Level 2
     sp2 = add_sp(story, 2, sp1,  "You see a bunch of folks, on left there's a gang of hot girls and on your right there's a gang of sexy boys. ",  "dev", "Go to the cafe",10, "mid", None)
-    sp3 = add_sp(story, 3, sp1,  "O no! you missed the party, all your friends were enjoying in that cave. Awesome!!", "dev", "Go to the registration office", 10, "end", None)
+    sp3 = add_sp(story, 3, sp1,  "You ask at the desk for your student ID, but sudenly you hear someone shouting your name.", "dev", "Go to the registration office", 10, "end", None)
 
     # Level 3
-    sp4 = add_sp(story, 4, sp2,  "Nah! the girls were not intersted, didn't entertain you. ", "dev", "Go to the left", 10, "mid", "bad")
-    sp5 = add_sp(story, 5, sp2,  "Ooh la la! all boys are drooling to see you.", "dev", "Go to the right", 10, "mid", None)
-    sp6 = add_sp(story, 6, sp2,  "No chance for you, you actually are pretty boring.", "dev", "Neither left or right. Just stright ahead.", 10, "end", "bad")
+    sp4 = add_sp(story, 4, sp2,  "Nah! the girls were not interested, didn't entertain you. ", "dev", "Go to the left", 10, "mid", "bad")
+    sp5 = add_sp(story, 5, sp2,  "Ooh la la! all boys are drooling at you.", "dev", "Go to the right", 10, "mid", None)
+
+    sp6 = add_sp(story, 6, sp3,  "O dear! Be adventurous and atlest a little vigilant. You cant keep missing opportunities like these. Never mind better try love luck next time.", "dev", "You ignore", 10, "end", None)
+    sp7 = add_sp(story, 7, sp3,  "Such a dreamer you are. It's fire alarm, you better run else you might get burnt.", "dev", "You turn with a smile thinking you found your new love", 10, "end", None)
 
     #Level 4
-    sp7 = add_sp(story, 7, sp4,  "", "dev", "Simply disheartened, and go to your lectures.", 10, "mid", None)
-    sp8 = add_sp(story, 8, sp4,  "Here! begins your love story. You take however you wish to take further.", "dev", "Walk towards a beautiful girl standing all alone who seems to be lost.", 5, "end", "bad")
+    sp8 = add_sp(story, 7, sp4,  "Love luck not in your favour. You better concentrate on your studies so atleast you can stabilise your career.", "dev", "Simply disheartened, and go to your lectures.", 10, "end", None)
+    sp9 = add_sp(story, 8, sp4,  "Here! begins your love story. You take however you wish to take further.", "dev", "Walk towards a beautiful girl standing all alone who seems to be lost.", 5, "end", "bad")
+
+
+    sp10 = add_sp(story, 9, sp5,  "Out of the bunch, one seems to be quite attractive, smart, intelligent and sensible. But doesn't seem to show much interst in you.", "dev", "Loving the attention", 5, "mid", none)
+    sp11 = add_sp(story, 10, sp5,  "Sorry, you lost the chance to explore any love. Better try next time!", "dev", "Ooh yuck! they are surely disgusting.", 10,"end", "bad")
 
     #Level 5
-    sp9 = add_sp(story, 9, sp5,  "Out of the bunch, one seems to be quite attractive, smart, intelligent and sensible. But doesn't seem to show much interst in you.", "dev", "Loving the attention", 5, "end", "bad")
-    sp10 = add_sp(story, 10, sp5,  "Sorry, you lost the chance to explore any love. Better try next time!", "dev", "Ooh yuck! they are surely disgusting.", 10,"end", "bad")
-
-    sp11 = add_sp(story, 11, sp9,  "He agrees and here starts your new love story. Let's see where you take this.", "dev", "you invite him for a date.", 5, "end", "bad")
-    sp12 = add_sp(story, 12, sp9,  "Bad luck! Another girl comes takes him sneakily and you keep looking.", "dev", "Decide to throw your charm and let him approach you.", 10, "end", "good")
+    sp12 = add_sp(story, 11, sp10,  "He agrees and here starts your new love story. Let's see where you take this.", "dev", "you invite him for a date.", 5, "end", "good")
+    sp13 = add_sp(story, 12, sp10,  "Bad luck! Another girl comes takes him sneakily and you keep looking.", "dev", "Decide to throw your charm and let him approach you.", 10, "end", "bad")
 
 
 # sid - story_id; n - name; desc - description
