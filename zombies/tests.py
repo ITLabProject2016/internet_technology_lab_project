b51dc8783f36614abfea54013007a2df5dd72db5
shallu gangwani <shallugangwani@gmail.com>
2016-03-19 14:02:06 +0000

    final test cases

--- zombies/tests.py
+++ zombies/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from zombies.models import Story,StoryPoint,UserProfile
 from django.core.urlresolvers import reverse
-from zombies.views import index
+from zombies.views import index,profile,statistics,survivors,index_min
 from zombies.views import about
 from django.core.urlresolvers import resolve
 from django.test import Client
@@ -22,11 +22,32 @@ class URLTests(TestCase):
             self.assertEqual(found.func, about)
 
 
+        def test_root_url_resolves_to_profile_url(self):
+            found = resolve('/zombies/profile/')
+            self.assertEqual(found.func, profile)
+
+
+        def test_root_url_resolves_to_index_min_view(self):
+            found = resolve('/zombies/index-min/')
+            self.assertEqual(found.func, index_min)
+
+
+        def test_root_url_resolves_to_statistics_view(self):
+            found = resolve('/zombies/statistics/')
+            self.assertEqual(found.func, statistics)
+
+
+        def test_root_url_resolves_to_survivors_view(self):
+            found = resolve('/zombies/survivors/')
+            self.assertEqual(found.func, survivors)
+
+
 
 
 # Test cases for Views
 
 class ViewTests(TestCase):
+
 
         def test_index_view_test_no_story(self):
            c = Client()
@@ -34,7 +55,8 @@ class ViewTests(TestCase):
            self.assertEquals(response.status_code,200)
            self.assertContains(response,"There are no stories present")
            self.assertContains(response, "Welcome to the campus, stranger. Where will the night take you?.")
- 
+
+
 
         def test_index_view_test_one_story(self):
 
@@ -87,6 +109,39 @@ class ViewTests(TestCase):
            self.assertContains(response, "You have reached the end. Congratulations!")
            self.assertContains(response, "Play the story again")
            self.assertContains(response, "Play another story")
+
+
+
+        def test_index_view_test_survivors_no_survivors(self):
+           c = Client()
+           response = c.get("/zombies/survivors/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response,"Hall of Survivors")
+           self.assertContains(response, "No users have registered. Zombies feel lonely...")
+
+
+        def test_index_view_test_statistics_no_stats(self):
+           c = Client()
+           response = c.get("/zombies/statistics/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response,"All you need to know about Zombies.")
+           self.assertContains(response,"There are no registered survivors on the campus.")
+           self.assertContains(response ,"The proportion of completed good and bad endings for each story.")
+
+
+
+        def test_index_view_test_about(self):
+           c = Client()
+           response = c.get("/zombies/about/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response,"Our game")
+           self.assertContains(response,"Ourselves")
+           self.assertContains(response,"Our project")
+           self.assertContains(response, "What was used to create the page")
+
+
+
+
 
 
 # Test cases for Models
