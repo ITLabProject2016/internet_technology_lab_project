d9f9e7b12c769ba6263004704d9811b70886b25c
shallu gangwani <shallugangwani@gmail.com>
2016-03-19 12:46:35 +0000

    test cases covering views, URL & Model

--- zombies/tests.py
+++ zombies/tests.py
@@ -4,10 +4,11 @@ from django.core.urlresolvers import reverse
 from zombies.views import index
 from zombies.views import about
 from django.core.urlresolvers import resolve
+from django.test import Client
 
 
 
-# Test cases for Views
+# Test cases for URL
 
 class URLTests(TestCase):
 
@@ -21,6 +22,72 @@ class URLTests(TestCase):
             self.assertEqual(found.func, about)
 
 
+        def test_index_view_test_no_story(self):
+           c = Client()
+           response = c.get("/zombies/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response,"There are no stories present")
+           self.assertContains(response, "Welcome to the campus, stranger. Where will the night take you?.")
+
+
+# Test cases for Views
+
+class ViewTests(TestCase):
+
+        def test_index_view_test_one_story(self):
+
+           story = Story(name="Zombies on Campus",visits=1,description='Zombies desciption',picture='testpic')
+           story.save()
+           c = Client()
+           response = c.get("/zombies/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response, "Welcome to the campus, stranger. Where will the night take you?.")
+           self.assertContains(response,"/zombies/story-point/1/1/")
+           self.assertContains(response,"Zombies on Campus")
+
+           num_stories =len(response.context['stories'])
+           self.assertEquals(num_stories,1)
+
+
+        def test_index_view_test_one_story_no_storyPoint(self):
+
+           story = Story(name="Zombies on Campus",visits=1,description='Zombies desciption',picture='testpic')
+           story.save()
+           c = Client()
+           response = c.get("/zombies/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response,"/zombies/story-point/1/1/")
+           num_stories =len(response.context['stories'])
+           self.assertEquals(num_stories,1)
+
+           response = c.get("/zombies/story-point/sid=1/spid=1/")
+           self.assertEquals(response.status_code,404)
+
+
+        def test_index_view_test_one_story_one_storyPoint(self):
+
+           story = Story(name="Zombies on Campus",visits=1,description='Zombies desciption',picture='testpic')
+           story.save()
+           storyPoint = StoryPoint(description='You are in the library',choiceText='yes',experience=10,story_type='start',main_story_id_id=1,visits=1,story_point_id=1,picture='testpic2')
+           storyPoint.save()
+
+
+           c = Client()
+           response = c.get("/zombies/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response,"/zombies/story-point/1/1/")
+           num_stories =len(response.context['stories'])
+           self.assertEquals(num_stories,1)
+
+           response = c.get("/zombies/story-point/1/1/")
+           self.assertEquals(response.status_code,200)
+           self.assertContains(response, "You are in the library")
+           self.assertContains(response, "You have reached the end. Congratulations!")
+           self.assertContains(response, "Play the story again")
+           self.assertContains(response, "Play another story")
+
+
+# Test cases for Models
 
 class StoryMethodTests(TestCase):
 
