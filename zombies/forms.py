f55102fbfd01eae51572a47ee35a37afa0806f2d
kostis <kostiskag@gmail.com>
2016-03-21 12:13:17 +0000

    comments text 3

--- zombies/forms.py
+++ zombies/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from zombies.models import UserProfile
 
 
-# To allow the user to upload a profile picture
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
--- zombies/urls.py
+++ zombies/urls.py
@@ -7,10 +7,8 @@ urlpatterns = patterns(
     url(r'^about/$', views.about, name='about'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^index-min/$', views.index_min, name='index-min'),
-    # Takes the main story line's ID, as well as specific SP's ID.
     url(r'^story-point/(?P<sid>\d+)/(?P<spid>\d+)/$', views.story_point, name='story-point'),
     url(r'^statistics/$', views.statistics, name='statistics'),
     url(r'^survivors/$', views.survivors, name='survivors'),
-    # Takes the username of a player of interest
     url(r'^survivor/(?P<username>.*)/$', views.survivor_profile, name='survivor_profile'),
 )
